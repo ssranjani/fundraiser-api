39947ce0b922c05298a76725a6193712753cbc9a
ssranjani <ranjaniblooms@gmail.com>
2023-12-18 09:38:35 -0500

    Add controller

---
+++ src/main/java/com/demo/fundraiser/controller/FundraiserController.java
@@ -0,0 +1,15 @@
+package com.demo.fundraiser.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/funds")
+public class FundraiserController {
+    @GetMapping("")
+    public ResponseEntity<Integer> getFunds(){
+        return  ResponseEntity.ok(56780);
+    }
+}
