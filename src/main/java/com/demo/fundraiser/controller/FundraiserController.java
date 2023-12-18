2fe3c6c3a8676faeb5efa90adf2db02531edb028
ssranjani <ranjaniblooms@gmail.com>
2023-12-18 10:52:44 -0500

    Enable CORS globally

--- src/main/java/com/demo/fundraiser/controller/FundraiserController.java
+++ src/main/java/com/demo/fundraiser/controller/FundraiserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/funds")
 public class FundraiserController {
     @GetMapping("")
